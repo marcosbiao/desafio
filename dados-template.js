3f3532589a4b5b15fac531c5a97af6430604bb9b
Marcos Biao <marcosbiao@yahoo.com.br>
2026-05-24 18:19:20 -0300

    atualização

---
+++ dados-template.js
@@ -0,0 +1,25 @@
+window.desafioModelo = {
+  // Copie este arquivo para algo como dados-novo-desafio.js.
+  // Depois atualize o DATA_FILE e o DATA_OBJECT em template-desafio.html.
+  ultimaAtualizacao: "2026-05-24T00:00:00",
+
+  participantes: [
+    {
+      id: "participante-1",
+      nome: "Participante 1",
+      foto: "",
+    },
+    {
+      id: "participante-2",
+      nome: "Participante 2",
+      foto: "",
+    },
+  ],
+
+  // Para desafio de tempo, use valor em minutos.
+  // Para desafio de pontos, use valor como pontuacao.
+  registros: [
+    { data: "2026-05-24", participante: "participante-1", valor: 30 },
+    { data: "2026-05-24", participante: "participante-2", valor: 25 },
+  ],
+};
