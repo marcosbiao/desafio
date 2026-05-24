2c7412b20383714da3b1ba2311f682b3422dff67
Marcos Biao <marcosbiao@yahoo.com.br>
2026-05-24 09:13:23 -0300

    atualização

--- dados.js
+++ dados.js
@@ -33,6 +33,12 @@ window.desafioFitness = {
     { data: "2026-05-21", aluna: "talita", tempo: 56 },
     { data: "2026-05-20", aluna: "laura", tempo: 30 },
     { data: "2026-05-21", aluna: "laura", tempo: 45 },
+    { data: "2026-05-22", aluna: "laura", tempo: 50 },
+    { data: "2026-05-24", aluna: "laura", tempo: 15 },
+    { data: "2026-05-22", aluna: "talita", tempo: 43 },
+    { data: "2026-05-23", aluna: "talita", tempo: 60 },
+    { data: "2026-05-22", aluna: "san", tempo: 50 },
+    { data: "2026-05-24", aluna: "san", tempo: 60 },
 
 
   ],
